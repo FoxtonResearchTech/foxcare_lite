ddf8fa25d467e7c88cd7f44727064d62b641f67f
NishanthDeveloper <your-email@example.commyprojects061312@gmail.com>
2025-01-08 11:42:41 +0530

    pharmesy dashboard

--- lib/presentation/dashboard/pharmecy_dashboard.dart
+++ lib/presentation/dashboard/pharmecy_dashboard.dart
@@ -1,5 +1,11 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:foxcare_lite/presentation/billings/counter_sales.dart';
+import 'package:foxcare_lite/presentation/billings/ip_billing.dart';
+import 'package:foxcare_lite/presentation/billings/medicine_return.dart';
+import 'package:foxcare_lite/presentation/tools/manage_pharmacy_info.dart';
+import 'package:foxcare_lite/utilities/colors.dart';
+import 'package:foxcare_lite/utilities/widgets/appBar/app_bar.dart';
 
 class SalesChartScreen extends StatelessWidget {
   const SalesChartScreen({Key? key}) : super(key: key);
@@ -7,40 +13,153 @@ class SalesChartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Sales Data Line Chart'),
-      ),
-      body:Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SalesLineChart(),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              physics: const ScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: [
-                  GradientBox(
-                    gradientColors: [Colors.blue, Colors.lightBlueAccent, Colors.cyan],
-                    text: 'Hello!',
-                  ),
-                  SizedBox(height: 20),
-                  GradientBox(
-                    gradientColors: [Colors.red, Colors.orange, Colors.yellow],
-                    text: 'Amazing!',
-                  ),
-                ],
+        backgroundColor: Colors.white,
+        appBar: CustomAppBar(
+          backgroundColor: AppColors.appBar,
+          fieldNames: [
+            'Home',
+            'Billing',
+            'Stock Management',
+            'Reports',
+            'Tools'
+          ],
+          navigationMap: {
+            'Billing': {
+              'Counter Sales': (context) => CounterSales(),
+              'OP Billings': (context) => CounterSales(),
+              'Bill Canceling': (context) => ManagePharmacyInfo(),
+              'Medicine Return': (context) => MedicineReturn(),
+              'IP Billing': (context) => IPBilling(),
+            }
+          },
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SalesLineChart(),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const ScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    GradientBox(
+                      subText: "25",
+                      gradientColors: [
+                        Colors.blue,
+                        Colors.lightBlueAccent,
+                        Colors.cyan
+                      ],
+                      text: 'No of Bill Generated',
+                    ),
+                    SizedBox(height: 20),
+                    GradientBox(
+                      subText: '15',
+                      gradientColors: [
+                        Colors.red,
+                        Colors.orange,
+                        Colors.yellow
+                      ],
+                      text: 'Total Qty Sales',
+                    ),
+                    SizedBox(height: 20),
+                    GradientBox(
+                      subText: '₹150000',
+                      gradientColors: [
+                        Color(0xFF004D40), // Dark Green
+                        Color(0xFF00C853), // Emerald Green
+                        Color(0xFFB9F6CA), // Light Mint Green
+                      ],
+                      text: 'Total Amount Collected',
+                    ),
+                    SizedBox(height: 20),
+                    GradientBox(
+                      subText: '158',
+                      gradientColors: [
+                        Color(0xFF6A1B9A), // Deep Purple
+                        Color(0xFFAB47BC), // Medium Purple
+                        Color(0xFFE1BEE7), // Light Lavender
+                      ],
+                      text: 'Total Medicine Entry Qty',
+                    ),
+                    SizedBox(height: 20),
+                    GradientBox(
+                      subText: '₹452125',
+                      gradientColors: [
+                        Color(0xFFF06292), // Light Pink
+                        Color(0xFFE91E63), // Pink
+                        Color(0xFFD50000), // Deep Red
+                      ],
+                      text: 'Total Medicine \n Retrive Value',
+                    ),
+                    SizedBox(height: 20),
+                    GradientBox(
+                      subText: '₹845665',
+                      gradientColors: [
+                        Color(0xFF4DD0E1), // Light Cyan
+                        Color(0xFF00ACC1), // Medium Cyan
+                        Color(0xFF006064), // Deep Cyan
+                      ],
+                      text: 'Total Total Collection',
+                    ),
+                    SizedBox(height: 30),
+                    Center(
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              EdgeInsets.symmetric(
+                                  vertical: 15, horizontal: 30)),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          elevation: MaterialStateProperty.all(0),
+                          shape:
+                              MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7),
+                          )),
+                          shadowColor: MaterialStateProperty.all(
+                              Colors.black.withOpacity(0.3)),
+                        ),
+                        onPressed: () {
+                          // Add your action here when the button is pressed
+                        },
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Color(0xFF64B5F6), Color(0xFF2196F3)],
+                              // Gradient color
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 30),
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Non Moving Products',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontFamily:
+                                    'Roboto', // Use any custom font if you like
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 100,)
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      )
-
-    );
+          ],
+        ));
   }
 }
 
@@ -65,11 +184,33 @@ class SalesLineChart extends StatelessWidget {
     ];
 
     final List<double> sales2024 = [
-      1500, 2000, 1800, 2200, 3000, 3500, 3200, 2900, 3300, 4000, 4500, 5000
+      1500,
+      2000,
+      1800,
+      2200,
+      3000,
+      3500,
+      3200,
+      2900,
+      3300,
+      4000,
+      4500,
+      5000
     ];
 
     final List<double> sales2025 = [
-      1600, 2100, 1900, 2400, 3100, 3600, 3300, 3000, 3400, 4100, 4600, 5100
+      1600,
+      2100,
+      1900,
+      2400,
+      3100,
+      3600,
+      3300,
+      3000,
+      3400,
+      4100,
+      4600,
+      5100
     ];
 
     return Container(
@@ -158,7 +299,7 @@ class SalesLineChart extends StatelessWidget {
                   .map((e) => FlSpot(e.key + 1.0, e.value))
                   .toList(),
               isCurved: true,
-              color: Colors.green,
+              color: Colors.teal,
               barWidth: 4,
               isStrokeCapRound: true,
               belowBarData: BarAreaData(
@@ -174,7 +315,7 @@ class SalesLineChart extends StatelessWidget {
                   .map((e) => FlSpot(e.key + 1.0, e.value))
                   .toList(),
               isCurved: true,
-              color: Colors.red,
+              color: Colors.pink,
               barWidth: 4,
               isStrokeCapRound: true,
               belowBarData: BarAreaData(
@@ -190,22 +331,23 @@ class SalesLineChart extends StatelessWidget {
   }
 }
 
-
 class GradientBox extends StatelessWidget {
   final List<Color> gradientColors;
   final String text;
+  final String subText; // New text
 
   const GradientBox({
     Key? key,
     required this.gradientColors,
     required this.text,
+    required this.subText, // Accept new text as a parameter
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 150,
-      height: 150,
+      width: 300,
+      height: 300,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: gradientColors,
@@ -222,13 +364,26 @@ class GradientBox extends StatelessWidget {
         ],
       ),
       child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8), // Space between the two texts
+            Text(
+              subText,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
