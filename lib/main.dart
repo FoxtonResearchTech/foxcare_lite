184b5e42e96a22d5b339a72bf123a82a5f2bb2d1
NishanthDeveloper <your-email@example.commyprojects061312@gmail.com>
2025-01-09 10:51:09 +0530

    pharmesy dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:foxcare_lite/presentation/billings/bill_search.dart';
-import 'package:foxcare_lite/presentation/billings/counter_sales.dart';
-import 'package:foxcare_lite/presentation/billings/ip_billing.dart';
-import 'package:foxcare_lite/presentation/billings/medicine_return.dart';
-import 'package:foxcare_lite/presentation/billings/prescription_billing.dart';
-import 'package:foxcare_lite/presentation/login/login.dart';
-import 'package:foxcare_lite/presentation/reports/broken_or_damaged_statement.dart';
-import 'package:foxcare_lite/presentation/reports/collection_report.dart';
-import 'package:foxcare_lite/presentation/reports/expiry_return_statement.dart';
-import 'package:foxcare_lite/presentation/reports/non_moving_stock.dart';
-import 'package:foxcare_lite/presentation/reports/party_wise_statement.dart';
-import 'package:foxcare_lite/presentation/reports/pending_payment_report.dart';
-import 'package:foxcare_lite/presentation/reports/product_wise_statement.dart';
-import 'package:foxcare_lite/presentation/reports/stock_management.dart';
-import 'package:foxcare_lite/presentation/reports/stock_return_statement.dart';
-import 'package:foxcare_lite/presentation/stock_management/add_product.dart';
-import 'package:foxcare_lite/presentation/stock_management/cancel_bill.dart';
-import 'package:foxcare_lite/presentation/stock_management/damage_return.dart';
-import 'package:foxcare_lite/presentation/stock_management/delete_product.dart';
-import 'package:foxcare_lite/presentation/stock_management/expiry_return.dart';
-import 'package:foxcare_lite/presentation/stock_management/product_list.dart';
-import 'package:foxcare_lite/presentation/stock_management/purchase.dart';
-import 'package:foxcare_lite/presentation/stock_management/purchase_entry.dart';
-import 'package:foxcare_lite/presentation/stock_management/purchase_order.dart';
-import 'package:foxcare_lite/presentation/stock_management/stock_return.dart';
-import 'package:foxcare_lite/presentation/tools/add_new_distributor.dart';
-import 'package:foxcare_lite/presentation/tools/distributor_list.dart';
-import 'package:foxcare_lite/presentation/tools/distributor_update.dart';
-import 'package:foxcare_lite/presentation/tools/manage_pharmacy_info.dart';
-import 'package:foxcare_lite/presentation/tools/pharmacist_list.dart';
-import 'package:foxcare_lite/presentation/tools/pharmacy_info.dart';
-import 'package:foxcare_lite/presentation/tools/profile.dart';
-import 'package:window_manager/window_manager.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'presentation/dashboard/pharmecy_dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
